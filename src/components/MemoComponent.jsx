8cefb838f3a3092a7f5291dd3dae3cbd521f25a5
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-10-06 11:24:13 +0330

    usememo for handling slow dunction to avoid unnecessary rendering is done.

--- src/components/MemoComponent.jsx
+++ src/components/MemoComponent.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import ChildComponent from "./ChildComp";
 
 const MemoComponent = () => {
   const [counter, setCounter] = useState(0);
   const [number, setNumber] = useState(0);
   console.log("Memo comp is rendered");
+  const slow = useMemo(() => {
+    console.log("for loop rendered");
+    for (let index = 0; index < 50000000; index++) {}
+    return counter;
+  }, [counter]);
   return (
     <div>
       <h3>Memo component</h3>
-      <span>{counter}</span>
+      <span>{slow}</span>
       <button onClick={() => setCounter((counter) => counter + 1)}>+</button>
       <ChildComponent number={number} />
       <span>{number}</span>
