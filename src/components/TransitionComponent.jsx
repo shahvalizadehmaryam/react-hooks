0897c49a17e842f4af98e580efd9c220d1ff6bce
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-10-12 11:17:33 +0330

    isPending logic added to show loading if all setstate changes didn't done competely.

--- src/components/TransitionComponent.jsx
+++ src/components/TransitionComponent.jsx
@@ -10,19 +10,25 @@ const TransitionComponent = () => {
   console.log("Transition comp rendered");
   const [value, setValue] = useState("");
   const [counter, setCounter] = useState(arrayCreator());
-  const [isPending , startTransition] = useTransition();
+  const [isPending, startTransition] = useTransition();
   const changeHandler = (event) => {
     setValue(event.target.value);
-    
-    startTransition(() => setCounter(arrayCreator(event.target.value)))
+
+    startTransition(() => setCounter(arrayCreator(event.target.value)));
   };
   return (
     <div>
       <h3>Transition comp</h3>
       <input type="text" value={value} onChange={changeHandler} />
-      {counter.map((i) => (
-        <p key={i}>{i}</p>
-      ))}
+      {isPending ? (
+        <h3>Loading...</h3>
+      ) : (
+        <>
+          {counter.map((i) => (
+            <p key={i}>{i}</p>
+          ))}
+        </>
+      )}
     </div>
   );
 };
